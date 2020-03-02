3a666e7be964401344e3d056323d4b569ba69855
mahmoodSherif <ma13121999@gmail.com>
2020-03-02 15:10:10 +0200

    add task view interface

---
+++ src/interfaces/todoView.js
@@ -0,0 +1,63 @@
+const priorityColors = ["green","#c9c300","red"];
+
+const getCompletedTaskView = (todo)=>{
+    const row = document.createElement('li');
+    row.classList.add("row");
+
+    const title = document.createElement("span");
+    title.classList.add("title");
+
+    title.style.textDecoration = "line-through";
+    title.innerText = todo.getTitle();
+
+    row.append(title);
+    return row;
+};
+
+const getSubTasksViews = (todo)=>{
+    const subList = document.createElement("ul");
+    let subTasks = todo.getChecklist();
+
+    for(let subTaskKey in subTasks){
+        let subTask = subTasks[subTaskKey];
+        const subTaskView = document.createElement("li");
+        subTaskView.classList.add("row");
+        subTaskView.innerText = subTask.getTitle();
+        subList.append(subTaskView);
+    }
+    return subList;
+};
+
+export const getTaskView = (todo)=>{
+
+    if(todo.getChecked()){
+        return getCompletedTaskView(todo);
+    }
+
+    const row = document.createElement('li');
+    row.classList.add("row");
+
+    const title = document.createElement("span");
+    title.classList.add("title");
+
+    const notes = document.createElement("span");
+    notes.classList.add("note");
+
+
+    title.innerText = todo.getTitle();
+    title.style.color = priorityColors[todo.getPriority() -1];
+
+    row.append(title);
+
+    if(todo.getNotes()){
+        notes.innerText = todo.getNotes();
+        row.append(notes);
+    }
+    
+    let x = Object.keys(todo.getChecklist()).length;
+    if(x){
+        row.append(getSubTasksViews(todo));
+    }
+
+    return row;
+};
