7ff2e33c12ce822d2d3c18da1370711f0e4f99ee
mahmoodSherif <ma13121999@gmail.com>
2020-04-11 23:13:10 +0200

    edit the listView

--- src/interfaces/listView.js
+++ src/interfaces/listView.js
@@ -1,7 +1,24 @@
-export function getListView(taskList){
-    let view = document.createElement("div");
-    view.classList.add("row");
-    view.setAttribute("data-id", taskList.getId());
-    view.innerText = taskList.getTitle();
-    return view;
+export function getListView(taskList) {
+    // list
+    const listView = document.createElement("div");
+    listView.classList.add("list");
+    listView.setAttribute("id", taskList.getId());
+
+    // title
+    const titleView = document.createElement("div");
+    titleView.classList.add("list-name");
+    titleView.innerText = taskList.getTitle();
+    listView.append(titleView);
+
+    // tasks
+    const tasksView = document.createElement("div");
+    tasksView.classList.add("tasks");
+    listView.append(tasksView);
+
+    // newTask
+    const newTaskView = document.createElement("div");
+    newTaskView.classList.add("newTask");
+    listView.append(newTaskView);
+
+    return listView;
 }
