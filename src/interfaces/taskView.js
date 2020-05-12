3cf8c5c0a16ef3ad5ae5fe6c322846582b40396e
mahmoodSherif <ma13121999@gmail.com>
2020-05-12 09:30:57 +0200

    remove unused code

--- src/interfaces/taskView.js
+++ src/interfaces/taskView.js
@@ -1,34 +1,3 @@
-const priorityColors = ['green', '#c9c300', 'red'];
-
-const getCompletedTaskView = (todo) => {
-    const row = document.createElement('li');
-    row.classList.add('Task');
-
-    const title = document.createElement('span');
-    title.classList.add('title');
-    title.setAttribute('data-id', todo.getId());
-
-    title.style.textDecoration = 'line-through';
-    title.innerText = todo.getTitle();
-
-    row.append(title);
-    return row;
-};
-
-const getSubTasksViews = (todo) => {
-    const subList = document.createElement('ul');
-    let subTasks = todo.getChecklist();
-
-    for (let subTaskKey in subTasks) {
-        let subTask = subTasks[subTaskKey];
-        const subTaskView = document.createElement('li');
-        subTaskView.classList.add('Task');
-        subTaskView.innerText = subTask.getTitle();
-        subList.append(subTaskView);
-    }
-    return subList;
-};
-
 export function getTaskView(task) {
     // task
     const taskView = document.createElement('div');
