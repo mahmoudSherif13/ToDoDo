127409d2de3b674ef780d01bc2bf5155d5345d38
mahmoodSherif <ma13121999@gmail.com>
2020-05-12 14:39:23 +0200

    add edit task form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,128 @@
-import { init } from './handlers/handle';
+import { List } from './classes/list';
+import { Task } from './classes/task';
+
+let lists = {};
+let tasks = {};
+let curId = -1;
+
+const listsHolder = document.getElementById('listsHolder');
+const taskIdView = document.getElementById("taskId");
+const newListBtn = document.getElementById('newList');
+const titleView = document.getElementById('listTitle');
+const editTaskPriority = document.getElementById("editTaskPriority");
+const editTaskText = document.getElementById("editTaskText");
+const editFormView = document.querySelector('[name="editTask"]');
+
+function createNewList(title) {
+    const list = List(title);
+    const listView = getListView(list);
+
+    lists[list.getId()] = list;
+    listsHolder.append(listView);
+}
+
+function createTask(title, listId) {
+    const task = Task(title);
+    const taskView = getTaskView(task);
+    const listTasks = document.getElementById(listId).querySelector('.tasks');
+
+    tasks[task.getId()] = task;
+    listTasks.append(taskView);
+}
+
+function deleteTask(TaskId) {
+    document.getElementById(TaskId).remove();
+}
+
+function editTask(Task) {
+    // edit view
+    const taskView = document.getElementById(curId);
+    taskView.querySelector(".taskName").innerHTML = Task.getTitle();
+
+    tasks[curId] = Task;
+}
+
+function editTaskForm(){
+    console.log("form sub");
+    const title = editTaskText.value;
+    const priority = editTaskPriority.value;
+    const edTask = Task(title);
+    edTask.setPriority(priority);
+    editTask(edTask);
+    editFormView.style.visibility = "hidden";
+}
+
+function init() {
+    newListBtn.addEventListener('click', () => {
+        const title = titleView.value;
+        createNewList(title);
+    });
+    editFormView.addEventListener("submit",editTaskForm);
+}
+
+function getTaskView(task) {
+    // task
+    const taskView = document.createElement('div');
+    taskView.classList.add('task');
+    taskView.setAttribute('id', task.getId());
+
+    // Priority
+    /// TODO : set the color based on the Priority
+    const PriorityView = document.createElement('span');
+    PriorityView.classList.add('priority');
+    taskView.append(PriorityView);
+
+    // title
+    const titleView = document.createElement('div');
+    titleView.classList.add("taskName");
+    titleView.innerText = task.getTitle();
+    taskView.append(titleView);
+
+    taskView.addEventListener('dblclick',()=>{
+        deleteTask(task.getId());
+    });
+
+    taskView.addEventListener('click',()=>{
+        curId = taskIdView.innerText = task.getId();
+        editFormView.style.visibility = "visible";
+        console.log(curId);
+    });
+
+    return taskView;
+}
+
+// list
+function newTaskBtn(id) {
+    let title = prompt('task title');
+    createTask(title, id);
+}
+function getListView(list) {
+    // list
+    const listView = document.createElement('div');
+    listView.classList.add('list');
+    listView.id = list.getId();
+
+    // title
+    const titleView = document.createElement('div');
+    titleView.classList.add('list-name');
+    titleView.innerText = list.getTitle();
+    listView.append(titleView);
+
+    // tasks
+    const tasksView = document.createElement('div');
+    tasksView.classList.add('tasks');
+    listView.append(tasksView);
+
+    // newTask
+    const newTaskView = document.createElement('div');
+    newTaskView.classList.add('newTask');
+    newTaskView.innerText = '+';
+    newTaskView.addEventListener('click', (e) =>
+        newTaskBtn(e.target.parentElement.id)
+    );
+    listView.append(newTaskView);
+
+    return listView;
+}
+
 init();
--- src/interfaces/storgeInterface.js
+++ src/interfaces/storgeInterface.js
@@ -1,42 +1,40 @@
-let taskId = 0;
-let subTaskId = 0;
-let listId = 0;
+let id = 0;
 
 export let lists = {};
 
-let keys = {
-    TASK_ID: 'taskId',
-    SUB_TASK_ID: 'subTaskId',
-    LIST_ID: 'listId',
-    LIST: 'list',
-};
+// let keys = {
+//     TASK_ID: 'taskId',
+//     SUB_TASK_ID: 'subTaskId',
+//     LIST_ID: 'listId',
+//     LIST: 'list',
+// };
 
 export function genTaskId() {
-    return taskId++;
+    return id++;
 }
 
 export function genSubTaskId() {
-    return subTaskId++;
+    return id++;
 }
 
 export function genListId() {
-    return listId++;
+    return id++;
 }
 
-export function save() {
-    localStorage.setItem(keys.TASK_ID, JSON.stringify(taskId));
-    localStorage.setItem(keys.SUB_TASK_ID, JSON.stringify(subTaskId));
-    localStorage.setItem(keys.LIST_ID, JSON.stringify(listId));
-    localStorage.setItem(keys.LIST, JSON.stringify(lists));
-}
+// export function save() {
+//     localStorage.setItem(keys.TASK_ID, JSON.stringify(taskId));
+//     localStorage.setItem(keys.SUB_TASK_ID, JSON.stringify(subTaskId));
+//     localStorage.setItem(keys.LIST_ID, JSON.stringify(listId));
+//     localStorage.setItem(keys.LIST, JSON.stringify(lists));
+// }
 
-export function load() {
-    if (!localStorage.getItem(keys.LIST)) {
-        return;
-    }
-    taskId = JSON.parse(localStorage.getItem(keys.TASK_ID));
-    subTaskId = JSON.parse(localStorage.getItem(keys.SUB_TASK_ID));
-    listId = JSON.parse(localStorage.getItem(keys.LIST_ID));
-    lists = JSON.parse(localStorage.getItem(keys.LIST));
-    console.log(`gamed ${taskId}\n`);
-}
+// export function load() {
+//     if (!localStorage.getItem(keys.LIST)) {
+//         return;
+//     }
+//     taskId = JSON.parse(localStorage.getItem(keys.TASK_ID));
+//     subTaskId = JSON.parse(localStorage.getItem(keys.SUB_TASK_ID));
+//     listId = JSON.parse(localStorage.getItem(keys.LIST_ID));
+//     lists = JSON.parse(localStorage.getItem(keys.LIST));
+//     console.log(`gamed ${taskId}\n`);
+// }
